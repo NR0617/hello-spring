1af0f5b03a4daafaeb1bdb4c5ed957f76baff808
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-28 15:13:34 +0900

    feat : 스프링 웹개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,49 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!");
+        model.addAttribute("data", "hello!");
         return "hello"; //hello.html에 model에 있는 데이터를 넘기고 화면을 렌더링 해라
+    }
+
+    @GetMapping("hello-mvc") //get으로 데이터 받기
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string") //get으로 데이터 받기
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); // 객체를 만든다
+        hello.setName(name); // 객체에 name이라는 프로퍼티를 넣는다
+        return hello; // 객체를 반환한다(반환의 디폴트는 json)
+    }
+
+    static class Hello {
+        private String name;
+
+        // 자바 빈 표준 방식 또는 프로퍼티 접근 방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
