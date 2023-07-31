40ded2b80e9ec8d86e0f42b37e48c71a6a38c9cf
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-31 16:57:01 +0900

    feat : 스프링 JdbcTemplate

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntTest.java
@@ -0,0 +1,33 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring!!!");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        Assertions.assertEquals(member.getName(), findMember.getName());
+
+    }
+
+}
