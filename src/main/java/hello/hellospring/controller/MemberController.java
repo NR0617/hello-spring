b9bf95a88f3359e11ef9b45f51ba42fe492e347c
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-08-01 17:29:00 +0900

    feat : AOP 연습

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -17,8 +17,8 @@ public class MemberController {
 
     @Autowired
     public MemberController(MemberService memberService){
-
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass());
     }
 
     @GetMapping("/members/new")
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 //JpaRepository가 구현체를 만들어서 스프링빈에 등록해준다-컨피그에 등록하자
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
+    //JPQL select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
 }
